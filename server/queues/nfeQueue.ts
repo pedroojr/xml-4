2d7cf5bbc6b8408c862d457163e6b2658e891f0d
pedroojr <48620395+pedroojr@users.noreply.github.com>
2025-08-26 01:49:41 -0500

    chore: run e2e tests with npx

--- server/queues/nfeQueue.ts
+++ server/queues/nfeQueue.ts
@@ -1,10 +1,13 @@
 import { Queue } from 'bullmq';
 
-const connection = {
-  host: process.env.REDIS_HOST || 'redis',
-  port: Number(process.env.REDIS_PORT || 6379),
-};
+export let nfeQueue: Queue | undefined;
+if (process.env.REDIS_HOST) {
+  const connection = {
+    host: process.env.REDIS_HOST,
+    port: Number(process.env.REDIS_PORT || 6379),
+  };
 
-export const nfeQueue = new Queue('nfeQueue', { connection });
+  nfeQueue = new Queue('nfeQueue', { connection });
+}
 
 export default nfeQueue;
