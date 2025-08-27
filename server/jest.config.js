feffba7a2f9d9163916903f911f6fb69686ad9ef
pedroojr <48620395+pedroojr@users.noreply.github.com>
2025-08-26 19:08:50 -0500

    test(server): fix module mocks and imports

--- server/jest.config.js
+++ server/jest.config.js
@@ -4,5 +4,8 @@ export default {
     '^.+\\.ts$': ['ts-jest', { useESM: true }],
   },
   extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
   testMatch: ['**/*.test.js', '**/*.test.ts'],
 };
